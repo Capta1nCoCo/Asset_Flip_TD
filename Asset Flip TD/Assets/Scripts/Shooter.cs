3e6d9aae72456f6f1659db8037ca0db4f9d9a52f
Capta1nCoCo <femabu28@gmail.com>
2020-09-10 22:09:16 +0300

    135. Instantiate From Gun Position

---
+++ Asset Flip TD/Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] Transform projectileOriginPoint;
+
+    public void Fire()
+    {
+        Instantiate(projectilePrefab, projectileOriginPoint.position, Quaternion.identity);
+    }
+}
