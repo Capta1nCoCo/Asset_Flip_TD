8765edcfbcec7c874b618b5ec232478a4b12d5b6
Capta1nCoCo <femabu28@gmail.com>
2020-09-10 20:11:10 +0300

    132. Spawn Attackers Using Coroutine

---
+++ Asset Flip TD/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] Attacker attackerPrefab;
+    [Header("Random.Range")]
+    [Tooltip("In Seconds")] [SerializeField] float minSpawnDelay = 1f;
+    [Tooltip("In Seconds")] [SerializeField] float maxSpawnDelay = 5f;
+
+    bool canSpawn = true;
+
+    IEnumerator Start()
+    {
+        while (canSpawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
