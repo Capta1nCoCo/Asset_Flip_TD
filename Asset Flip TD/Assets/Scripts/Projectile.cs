fdb2fbb202c817262be94c588d2ea7ed129ecc2a
Capta1nCoCo <femabu28@gmail.com>
2020-09-10 23:06:04 +0300

    136. Make Projectile Move

---
+++ Asset Flip TD/Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Range(0f, 100f)] [SerializeField] float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);        
+    }
+}
