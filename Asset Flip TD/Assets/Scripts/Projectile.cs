d6ff272d290373b035c94323a072ed41abcbb858
Capta1nCoCo <femabu28@gmail.com>
2020-09-11 16:25:13 +0300

    138. Implementing Health & Damage (Tuning TakeHit & Death Animations Indeed)

--- Asset Flip TD/Assets/Scripts/Projectile.cs
+++ Asset Flip TD/Assets/Scripts/Projectile.cs
@@ -7,13 +7,6 @@ public class Projectile : MonoBehaviour
     [Range(0f, 100f)] [SerializeField] float speed = 10f;
     [SerializeField] int damagePoints = 25;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);        
