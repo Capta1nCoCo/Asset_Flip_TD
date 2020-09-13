bb84074cfb3baa95e2419a22ba4ca31bfe655977
Capta1nCoCo <femabu28@gmail.com>
2020-09-13 20:50:33 +0300

    145. Start Our Resource System

---
+++ Asset Flip TD/Assets/Scripts/ManaDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ManaDisplay : MonoBehaviour
+{
+    [SerializeField] int mana = 100;
+    TextMeshProUGUI manaText;
+
+    void Start()
+    {
+        manaText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        manaText.text = mana.ToString();
+    }
+
+    public void AddMana(int amount)
+    {
+        mana += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendMana(int amount)
+    {
+        if (mana >= amount)
+        {
+            mana -= amount;
+            UpdateDisplay();
+        }
+        else
+        {
+            Debug.Log("I need more mana!");
+        }                
+    }
+
+}
