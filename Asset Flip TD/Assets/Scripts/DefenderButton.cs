4506d323e3c7c7eeb7c6ba3b5d59dc58aa5a8b53
Capta1nCoCo <femabu28@gmail.com>
2020-09-13 18:39:05 +0300

    143. Create Defender Selection UI

---
+++ Asset Flip TD/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{    
+    DefenderButton[] defenderButtons;
+    Color notSelected;
+
+    SpriteRenderer spriteRenderer;
+    
+    private void Start()
+    {
+        defenderButtons = FindObjectsOfType<DefenderButton>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        notSelected = spriteRenderer.color;        
+    }
+
+
+    private void OnMouseDown()
+    {
+        foreach (DefenderButton defenderButton in defenderButtons)
+        {
+            defenderButton.spriteRenderer.color = notSelected;
+        }
+        spriteRenderer.color = Color.white;
+    }
+   
+}
