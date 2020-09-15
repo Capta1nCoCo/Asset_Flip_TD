5db02a40b25fde4720ff6bb2a8cf7cd93d5155b9
Capta1nCoCo <femabu28@gmail.com>
2020-09-15 21:21:27 +0300

    152. Attacker In My Lane

--- Asset Flip TD/Assets/Scripts/Shooter.cs
+++ Asset Flip TD/Assets/Scripts/Shooter.cs
@@ -7,6 +7,53 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] Transform projectileOriginPoint;
 
+    AttackerSpawner myLaneSpawner;
+    
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Shoot Pew Pew!");
+            //TODO change animation state to Shooting
+        }
+        else
+        {
+            Debug.Log("Sit And Wait!");
+            //TODO change amimation state to Idle
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in attackerSpawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
     public void Fire()
     {
         Instantiate(projectilePrefab, projectileOriginPoint.position, Quaternion.identity);
