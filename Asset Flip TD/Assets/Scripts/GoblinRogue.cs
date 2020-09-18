403edf1419b3d3e875cfb5f1f5327cffdd717e20
Capta1nCoCo <femabu28@gmail.com>
2020-09-18 19:24:49 +0300

    159. Goblin Rogue & Monk Protector

---
+++ Asset Flip TD/Assets/Scripts/GoblinRogue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinRogue : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        if (otherObject.GetComponent<Shooter>())
+        {
+            Assasinate(otherObject);
+        }
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+
+    private void Assasinate(GameObject shooter)
+    {
+        GetComponent<Animator>().SetTrigger("Assasinate");
+        Destroy(shooter, 1f);
+        //TODO add blood explosion particles
+    }
+}
--- Asset Flip TD/Assets/Scripts/MonkProtector.cs
+++ Asset Flip TD/Assets/Scripts/MonkProtector.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonkProtector : MonoBehaviour
-{
-    [SerializeField] int damage = 25;
+{    
     Animator animator;
+    GameObject currentTarget;
 
     private void Awake()
     {
@@ -14,7 +14,34 @@ public class MonkProtector : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        animator.SetBool("isAttacking", true);
+        GameObject otherObject = collision.gameObject;
+        if (otherObject.GetComponent<Attacker>())
+        {
+            Attack(otherObject);
+        }
     }
 
+    public void Attack(GameObject target)
+    {
+        animator.SetBool("isAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(int damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.TakeHit(damage);
+        }
+    }
+
+    public void CheckCurrentTargetExistence()
+    {
+        if (currentTarget == null)
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
 }
