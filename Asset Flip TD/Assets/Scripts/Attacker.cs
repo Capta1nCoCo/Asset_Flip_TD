75ef8745c6351ca0cd0039a2714727224336d09b
Capta1nCoCo <femabu28@gmail.com>
2020-09-18 15:25:20 +0300

    158. Attackers Striking Target

--- Asset Flip TD/Assets/Scripts/Attacker.cs
+++ Asset Flip TD/Assets/Scripts/Attacker.cs
@@ -7,11 +7,17 @@ public class Attacker : MonoBehaviour
 {
     [Range (0f, 5f)] [SerializeField] float currentSpeed = 1f;
     GameObject currentTarget;
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
-    }
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);        
+    }    
 
     public void SetMovementSpeed(float speed)
     {
@@ -20,7 +26,27 @@ public class Attacker : MonoBehaviour
     
     public void Attack(GameObject target)
     {
-        GetComponent<Animator>().SetBool("isAttacking", true);
-        currentTarget = target;
+        animator.SetBool("isAttacking", true);
+        currentTarget = target;        
     }
+    
+    public void StrikeCurrentTarget(int damage)
+    {
+        if(!currentTarget) { return; }        
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.TakeHit(damage);            
+        }        
+    }
+
+    public void CheckCurrentTargetExistence()
+    {
+        if (currentTarget == null)
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+
 }
---
+++ Asset Flip TD/Assets/Scripts/MonkProtector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkProtector : MonoBehaviour
+{
+    [SerializeField] int damage = 25;
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetBool("isAttacking", true);
+    }
+
+}
